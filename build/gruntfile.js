68caa39d2c789dd9e0e8e46363b35cbf3731b6f3
Andreas Steffensen <andreas.steffensen89@gmail.com>
2019-04-19 16:56:22 +0200

    add susy and bourbon

--- build/gruntfile.js
+++ build/gruntfile.js
@@ -1,5 +1,29 @@
-//The "wrapper"
+// Gruntfile.js
 
-//Project and task configuration
-//Loading Grunt plugins and tasks
-//Custom tasks
+// our wrapper function (required by grunt and its plugins)
+// all configuration goes inside this function
+module.exports = function (grunt) {
+
+    // ===========================================================================
+    // CONFIGURE GRUNT ===========================================================
+    // ===========================================================================
+    grunt.initConfig({
+
+        // get the configuration info from package.json ----------------------------
+        // this way we can use things like name and version (pkg.name)
+        pkg: grunt.file.readJSON('package.json'),
+
+        // all of our configuration will go here
+
+    });
+
+    // ===========================================================================
+    // LOAD GRUNT PLUGINS ========================================================
+    // ===========================================================================
+    // we can only load these if they are in our package.json
+    // make sure you have run npm install so our app can find these
+    grunt.loadNpmTasks('grunt-sass"');
+    grunt.loadNpmTasks('grunt-contrib-watch"');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
