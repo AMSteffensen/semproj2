7b190611aae8e84958018a7a4c64ad9f238648e7
Andreas Steffensen <andreas.steffensen89@gmail.com>
2019-04-22 19:49:24 +0200

    added two player character selection

--- build/assets/js/characterSelect.js
+++ build/assets/js/characterSelect.js
@@ -1,8 +1,34 @@
 const cards = document.querySelectorAll('.character');
 
+let hasSelectedCharacter = false;
+let firstCharacter, secondCharacter;
+
 function selectCharacter() {
-    console.log('I was clicked');
-    console.log(this);
+    // console.log('I was clicked');
+    // console.log(this);
+    this.classList.toggle('character-select');
+
+    //first player select
+    if (!hasSelectedCharacter) {
+        hasSelectedCharacter = true;
+        firstCharacter = this;
+
+        console.log({
+            hasSelectedCharacter,
+            firstCharacter
+        });
+        console.log('Player one has selected.');
+    } else {
+        //second player select
+        hasSelectedCharacter = false;
+        secondCharacter = this;
+
+        console.log({
+            firstCharacter,
+            secondCharacter
+        });
+        console.log('Player two has selected.')
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', selectCharacter));
