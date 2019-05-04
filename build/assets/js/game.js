8701cec2bccd6e79a7050404d80bf1d6ce49f916
Andreas Steffensen <andreas.steffensen89@gmail.com>
2019-05-04 13:26:30 +0200

    add game.js

---
+++ build/assets/js/game.js
@@ -0,0 +1,28 @@
+ var x = 0;
+ var y = 0;
+
+ var elem = document.getElementById = "number";
+ var button = document.getElementById = "count";
+
+ function countClicks() {
+     console.log('Number is: ' + x);
+     x++;
+ }
+
+
+
+ function rollDice() {
+
+     //throw dice no number greater than 6
+     const maxNumber = 6;
+     var rollDice = Math.ceil(Math.random() * max);
+ }
+
+
+
+ //  document.addEventListener('DOMContentLoaded', function () {
+ //      document.getElementById('roll').onclick = function () {
+ //          var rollResult = 'test';
+ //          document.getElementsByClassName('message')[0].textContent = rollResult;
+ //      };
+ //  });
