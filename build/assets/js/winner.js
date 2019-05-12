1908c6a43122dbaa36f9a4f87aac46b7a80854f9
Andreas Steffensen <andreas.steffensen89@gmail.com>
2019-05-12 19:25:40 +0200

    refactoring

--- build/assets/js/winner.js
+++ build/assets/js/winner.js
@@ -8,52 +8,26 @@ var canvas = document.getElementById('canvas'),
     image = new Image(),
     winner = localStorage.getItem("Winner"),
     character = localStorage.getItem(winner);
-text = character;
-text2 = 'Won the game';
+text = character + ' Won the game';
 
-function draw() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawWinner();
+image.src = 'assets/images/' +
+    character + '.svg';
+context.clearRect(0, 0, canvas.width, canvas.height);
+
+
+image.onload = function (e) {
+    canvas.width = image.naturalWidth
+    canvas.height = image.naturalHeight
+    context.drawImage(image, 0, 0);
     drawText();
-}
-
-function drawWinner() {
-
-    image.src = 'assets/images/' +
-        character + '.svg';
-    context.clearRect(0, 0, canvas.width, canvas.height);
+};
 
 
-    image.onload = function (e) {
-        context.drawImage(image, 150, 250);
-    };
-
-}
 
 function drawText() {
-    var TEXT_X = 65,
-        TEXT_Y = canvas.height / 4 + 50;
-
-    var TEXT_X2 = 165,
-        TEXT_Y2 = canvas.height / 4 + 80;
-    context.strokeStyle = 'blue';
-
-    context.fillText(text, TEXT_X, TEXT_Y);
-    context.strokeText(text, TEXT_X, TEXT_Y);
-
-    context.fillText(text2, TEXT_X2, TEXT_Y2);
-    context.strokeText(text2, TEXT_X2, TEXT_Y2);
-
+    var box = document.getElementById('winner');
+    box.textContent = text;
 }
-
-context.font = '30px Palatino';
-context.lineWidth = 1.0;
-context.fillStyle = 'cornflowerblue';
-
-
-
-draw();
-
 
 //If player wants to try again, send back to character select screen
 function tryAgain() {
